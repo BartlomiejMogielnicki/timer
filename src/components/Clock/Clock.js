db2ef712a3008a0a6f745105da8d387b8f499d54
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-07-25 17:38:09 +0200

    Fix time format

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Clock = (props) => {
   const { hours, minutes, seconds } = props.time
   return (
-    <div>{hours}:{minutes}:{seconds}</div>
+    <div>
+      {hours < 10 ? `0${hours}` : hours}
+      :
+      {minutes < 10 ? `0${minutes}` : minutes}
+      :
+      {seconds < 10 ? `0${seconds}` : seconds}
+    </div>
   )
 }
 
